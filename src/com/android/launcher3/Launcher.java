e381f949c28e0026eec668675e771cf925b26d99
Ali B <abittin@gmail.com>
2021-01-30 22:36:40 +0300

    Launcher3: Add uninstall button to system shortcuts
    [ghostrider-reborn: Adapt to Sv2, 13]
    
    Squashed:
    
    Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Date:   Sun Jun 26 18:44:18 2022 +0530
    
        Launcher3: SystemShortcut: Avoid NPE due to uninstall button
    
        Crash log: https://bin.kv2.dev/~62b85c7008bb984d9dc61e94
    
        Steps to reproduce:
        - Add Now Playing widget to home screen
        - Long press on it
        - Launcher crashes
    
        Test: manual; no longer crashes
        Change-Id: I6c0f73e38a3695a64d820c40b58f5e87ba7013c5
    
    Change-Id: I005d676d9a98f65296c330e5e13fd0d849df6fe5
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -91,6 +91,7 @@ import static com.android.launcher3.popup.SystemShortcut.ADD_TO_HOME_SCREEN;
 import static com.android.launcher3.popup.SystemShortcut.APP_INFO;
 import static com.android.launcher3.popup.SystemShortcut.INSTALL;
 import static com.android.launcher3.popup.SystemShortcut.REMOVE;
+import static com.android.launcher3.popup.SystemShortcut.UNINSTALL;
 import static com.android.launcher3.popup.SystemShortcut.WIDGETS;
 import static com.android.launcher3.states.RotationHelper.REQUEST_LOCK;
 import static com.android.launcher3.states.RotationHelper.REQUEST_NONE;
@@ -2828,7 +2829,7 @@ public class Launcher extends StatefulActivity<LauncherState>
     public Stream<SystemShortcut.Factory> getSupportedShortcuts(ItemInfo itemInfo) {
         int container = itemInfo.container;
         if (container == CONTAINER_DESKTOP || container == CONTAINER_HOTSEAT) {
-            return Stream.of(APP_INFO, WIDGETS, INSTALL, REMOVE);
+            return Stream.of(APP_INFO, WIDGETS, INSTALL, UNINSTALL, REMOVE);
         } else if (container == CONTAINER_ALL_APPS || container == CONTAINER_ALL_APPS_PREDICTION) {
             // TODO(b/444744861): Update private space apps to have its own container.
             boolean isPinnable = itemInfo instanceof ItemInfoWithIcon info
