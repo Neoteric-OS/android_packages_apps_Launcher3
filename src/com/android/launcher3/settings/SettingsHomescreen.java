bb584d4d9b8cf6769d3f7eb2c8b40da052c4c9ab
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-07-31 23:43:51 +0530

    Launcher3: Add a toggle for smartspace
    [DennySPb]: adapt to 14-qpr2
    [HELLBOY017]: adapt to 15
    
    Change-Id: I3b2681d51197642ba0957111db455937c0c376e8
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- src/com/android/launcher3/settings/SettingsHomescreen.java
+++ src/com/android/launcher3/settings/SettingsHomescreen.java
@@ -133,6 +133,7 @@ public class SettingsHomescreen extends CollapsingToolbarBaseActivity
             case Utilities.KEY_STATUS_BAR:
             case Utilities.KEY_SHORT_PARALLAX:
             case Utilities.KEY_SINGLE_PAGE_CENTER:
+            case Utilities.KEY_SMARTSPACE:
                 LauncherAppState.getInstance(this).setNeedsRestart();
                 break;
             default:
