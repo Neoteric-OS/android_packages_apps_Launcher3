46e2ffdf577a6c8f1da4b1db38f0c42fd2f7de67
StarkDroid <gamerprince.exp@gmail.com>
2022-11-29 15:48:29 +0800

    Launcher3: Add toggle to show/hide app drawer search bar
    Change-Id: I66bb9a232e7e49a94a1c8b11bea195a5d8faa6d1
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -404,7 +404,11 @@ public class Launcher extends StatefulActivity<LauncherState>
     private final OnSharedPreferenceChangeListener mSharedPrefListener =
             new OnSharedPreferenceChangeListener() {
                 @Override
-                public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {}
+                public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+                    if (Utilities.KEY_DRAWER_SEARCH.equals(key)) {
+                        mNeedsRestart = true;
+                    }
+                }
             };
 
     private InputMethodManager mInputMethodManager;
