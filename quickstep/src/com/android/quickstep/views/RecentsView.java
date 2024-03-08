fdbbd68d0725fd269f28b460189652f3bb521433
PugzAreCute <me@pugzarecute.com>
2024-03-08 20:32:22 +0530

    Launcher3: Skip overview scale animation on tablet
    * Calling doScrollScale() on a tablet causes the app to become "small" and then expands to fit the screen when scrolling
    * Bug has been reproduced on nabu(Xiaomi Pad 5), pipa(Xiaomi Pad 6) and gta4xl(Samsung Tab S6 Lite)
    * Original patch was by kfkcome(https://github.com/Kfkcome/android_packages_apps_Launcher3/commit/69c0319666cbb84e34a65cb770c4c4188253d50c)
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I0cfbaf7d7b3adb73efefe3a77b863c0a7df1e959
    (cherry picked from commit 27651fbe10145aef850adc1da2f3cb0678054108)
    
    markakash: update for Android 17
    Change-Id: I9aba09b2eba16b8f9661aefc33f87ca0a3a27102

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -5757,7 +5757,7 @@ public abstract class RecentsView<
     }
 
     private void doScrollScale() {
-        if (showAsGrid())
+        if (showAsGrid() || mContainer.getDeviceProfile().getDeviceProperties().isLargeScreen())
             return;
 
         boolean isInLandscape = mOrientationState.getTouchRotation() != ROTATION_0
