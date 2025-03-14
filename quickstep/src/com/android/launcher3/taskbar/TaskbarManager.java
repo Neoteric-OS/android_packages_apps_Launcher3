5f1f0eeb697160aa420389dd0c5be2208c666ce3
LuK1337 <priv.luk@gmail.com>
2025-03-14 22:26:47 +0100

    Launcher3: Implement taskbar navbar inversion
    Change-Id: I8f8d8226d4c332a61956126238a7607eda66faf4

--- quickstep/src/com/android/launcher3/taskbar/TaskbarManager.java
+++ quickstep/src/com/android/launcher3/taskbar/TaskbarManager.java
@@ -142,6 +142,9 @@ public class TaskbarManager implements DisplayDecorationListener {
     private static final Uri ENABLE_TASKBAR_URI = Settings.System.getUriFor(
             Settings.System.ENABLE_TASKBAR);
 
+    public static final Uri NAV_BAR_INVERSE = Settings.Secure.getUriFor(
+            "sysui_nav_bar_inverse");
+
     private final Context mBaseContext;
     private final int mPrimaryDisplayId;
     private final TaskbarNavButtonCallbacks mNavCallbacks;
@@ -461,6 +464,8 @@ public class TaskbarManager implements DisplayDecorationListener {
                 .register(USER_SETUP_COMPLETE_URI, mOnSettingsChangeListener);
         SettingsCache.INSTANCE.get(mPrimaryWindowContext)
                 .register(NAV_BAR_KIDS_MODE, mOnSettingsChangeListener);
+        SettingsCache.INSTANCE.get(mPrimaryWindowContext)
+                .register(NAV_BAR_INVERSE, mOnSettingsChangeListener);
         mEnableTaskBarListener = c -> {
             // Create the illusion of this taking effect immediately
             // Also needed because TaskbarManager inits before SystemUiProxy on start
@@ -1127,6 +1132,8 @@ public class TaskbarManager implements DisplayDecorationListener {
                 .unregister(USER_SETUP_COMPLETE_URI, mOnSettingsChangeListener);
         SettingsCache.INSTANCE.get(mPrimaryWindowContext)
                 .unregister(NAV_BAR_KIDS_MODE, mOnSettingsChangeListener);
+        SettingsCache.INSTANCE.get(mPrimaryWindowContext)
+                .unregister(NAV_BAR_INVERSE, mOnSettingsChangeListener);
         SystemDecorationChangeObserver.getINSTANCE().get(mPrimaryWindowContext)
                 .unregisterDisplayDecorationListener(this);
         debugPrimaryTaskbar("destroy: unregistering component callbacks");
